5171ad569c7e3f89ccfb4680eb6145132ed89cfd
ferat.goksu <ferat.goksu@ibb.gov.tr>
2024-07-18 16:41:55 +0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header"
+import Results from "./components/Results"
+import UserInput from "./components/UserInput"
 function App() {
+
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = Object.values(userInput).every((value) => value > 0);
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+    <Header />
+    <UserInput userInput={userInput} onChange={handleChange} />
+    {!inputIsValid && <p className="center">Please fill out all fields</p>}
+     {inputIsValid && <Results input={userInput} />}  
+    </>
   )
 }
 
